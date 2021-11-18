0b457d0a6dba7d9d72a3acef58c29fd900f021b3
Matt <matthiasvstephens@gmail.com>
2021-11-18 15:41:20 +0100

    Prepare for heroku

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -2,19 +2,16 @@ const DATA_CACHE = 'data-cache-1';
 
 const FILES_TO_CACHE = [
   '/',
-  '/index.html',
-  '/index.js',
-  '/indexedDb.js',
-  '/styles.css',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
-  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+  './index.html',
+  './index.js',
+  './indexedDb.js',
+  './styles.css',
 ];
 
-self.addEventListener('install', async (e) => {
-  const cache = await e.waitUntil(caches.open(DATA_CACHE));
-  cache.addAll(FILES_TO_CACHE);
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(DATA_CACHE).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
   self.skipWaiting();
 });
 

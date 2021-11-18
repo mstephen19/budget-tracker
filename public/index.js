da9b1e9f2fd7aeb455161c39e9c2f1c9d16f985d
Matt <matthiasvstephens@gmail.com>
2021-11-18 14:50:32 +0100

    Add CheckIfIndexedDbSupported function

--- public/index.js
+++ public/index.js
@@ -136,8 +136,8 @@ function sendTransaction(isAdding) {
     })
     .catch((err) => {
       // fetch failed, so save in indexed db
+      if (!checkIfIndexedDbSupported()) return alert('Failed to add!');
       saveRecord(transaction);
-      // clear form
       nameEl.value = '';
       amountEl.value = '';
     });

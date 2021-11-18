e7857245f7c6bc015d31383035243039855a8e22
Matt <matthiasvstephens@gmail.com>
2021-11-18 14:47:43 +0100

    Small refactor in IndexedDb.js

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -40,19 +40,18 @@ function checkDb() {
   const allInDb = store.getAll();
 
   allInDb.onsuccess = async () => {
-    if (allInDb.result.length > 0) {
-      const response = await fetch('/api/transaction/bulk', {
-        method: 'POST',
-        body: JSON.stringify(allInDb.result),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      if (!response.ok) return;
-      const transaction = db.transaction(['budgetTracker'], 'readwrite');
-      const store = transaction.objectStore('budgetTracker');
-      store.clear();
-    }
+    if (allInDb.result.length === 0) return;
+    const response = await fetch('/api/transaction/bulk', {
+      method: 'POST',
+      body: JSON.stringify(allInDb.result),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) return;
+    const transaction = db.transaction(['budgetTracker'], 'readwrite');
+    const store = transaction.objectStore('budgetTracker');
+    store.clear();
   };
 }
 
